2989d652fee7383e669803b2483c24efa2d72bf7
ghoshshreya <shrghosh08@gmail.com>
2023-08-20 00:14:38 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { from, map, of, tap } from 'rxjs';
 let values = ['Penny', 'Bernadette', 'Amy'];
 
 let obs = from(values).pipe(
+  //map
   tap((x) => {
     console.log('Current Value', x);
     return 'Updated - ' + x;
